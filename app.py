36a03e104c5ddad17a7f4f9731110104b6113ad4
gaojunran <nebula2021@126.com>
2025-06-13 00:13:25 +0800

    fix: change env name

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ def test():
 @app.get("/api/blog/test-mihome")
 def test_mihome():
     url = "http://localhost:8081/api/services/light/toggle"
-    token = os.getenv("MIHOME_TOKEN")
+    token = os.getenv("HOMEASSISTANT_TOKEN")
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
     }
     data = {"entity_id": "light.philips_cn_274401208_sread2_s_2_light"}
     response = requests.post(url, headers=headers, json=data)
-    return {"code": response.status_code, "data": response.text}
+    return {"code": response.status_code, "data": response.text, "log": headers}
 
 
 if __name__ == "__main__":
