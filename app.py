baecbaff3413e3a8e5231c7a71247e17f006574f
gaojunran <nebula2021@126.com>
2025-06-13 00:02:56 +0800

    feat: add endpoint `test-mihome`

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
+import os
+
+import requests
+from dotenv import load_dotenv
 from robyn import Robyn
 
+load_dotenv()
 app = Robyn(__file__)
 
 
 @app.get("/api/test")
 def test():
     return "Hello World!"
+
+
+@app.get("/api/blog/test-mihome")
+def test_mihome():
+    url = "http://localhost:8123/api/services/light/toggle"
+    token = os.getenv("MIHOME_TOKEN")
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
+    data = {"entity_id": "light.philips_cn_274401208_sread2_s_2_light"}
+    response = requests.post(url, headers=headers, json=data)
+    return {"code": response.status_code, "data": response.text}
 
 
 if __name__ == "__main__":
