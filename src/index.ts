ecfe3ec6c19ec2785cc0dec6b7503155c914a6f6
gaojunran <nebula2021@126.com>
2025-09-23 23:36:12 +0800

    update lms

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { Elysia } from "elysia";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { blogApp } from "./modules/blog";
+import { lmsApp } from "./modules/lms";
 
 export const db = drizzle(process.env.DATABASE_URL!);
 
-const app = new Elysia().use(blogApp).listen(3000);
+const app = new Elysia().use(blogApp).use(lmsApp).listen(3000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
---
+++ src/modules/lms/index.ts
@@ -0,0 +1,21 @@
+import Elysia, { t } from "elysia";
+import { JSONFile } from "lowdb/node"
+import { Low } from "lowdb/lib";
+
+const adapter = new JSONFile("../lms-cqupt-subscribe/receivers.json")
+const db: any = new Low(adapter, {});
+
+export const lmsApp = new Elysia({ prefix: "/lms" })
+  .post(
+    "/subscribe",
+    async ({ body }) => {
+      await db.read();
+      db.data[body.email] = body.email;
+      return { status: "ok", message: "Subscribe successfully 🎉!" };
+    },
+    {
+      body: t.Object({
+        email: t.String(),
+      }),
+    },
+  );
